d3e547aef7a61f710273d6839d6df3061767e523
Fangzhengjie <fangzhengjie@cloudwalk.cn>
2020-12-16 17:55:20 +0800

    聊天室

---
+++ practice-demo/src/main/java/com/r92ad8/practice/chatroom/ChatRoomUser.java
@@ -0,0 +1,28 @@
+package com.r92ad8.practice.chatroom;
+
+//用户信息类
+public class ChatRoomUser{
+    private String name;
+    private String ip;
+
+    public ChatRoomUser(String name, String ip) {
+        this.name = name;
+        this.ip = ip;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+}
