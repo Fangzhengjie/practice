8a958d21e9c85173149a91eb36a9a88e75e2745e
Fangzhengjie <fangzhengjie@cloudwalk.cn>
2021-01-19 16:57:37 +0800

    ThreadPoolTaskExecutorTest.java

--- practice-multi-thread-demo/src/java/main/com/r92ad8/practice/multithread/threadpool/ThreadPoolTaskExecutorTest.java
+++ practice-multi-thread-demo/src/java/main/com/r92ad8/practice/multithread/threadpool/ThreadPoolTaskExecutorTest.java
@@ -1,4 +1,26 @@
 package com.r92ad8.practice.multithread.threadpool;
 
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 public class ThreadPoolTaskExecutorTest {
+
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+         final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.initialize();
+        //无返回值的任务用execute
+        executor.execute(()->{
+            System.out.println(Thread.currentThread().getName()+"\t is Running.....");
+        });
+
+        //有返回值的任务用submit
+        Future<String> future =executor.submit(()->{
+            System.out.println(Thread.currentThread().getName()+"\t is Running.....");
+            return "ok";
+        });
+        future.get();
+    }
 }
